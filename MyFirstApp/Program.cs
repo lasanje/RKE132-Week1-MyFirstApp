6070d7ffb35730795ddc48f1034427e21519ead6
SP <spukk@tktk.ee>
2023-02-11 19:41:53 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Security;
+
+Console.WriteLine("Enter your name:"); //Output
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+Console.WriteLine($"Hello, {userName}!"); //Output
